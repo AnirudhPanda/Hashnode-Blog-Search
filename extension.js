5a830591e196672be426f8296fec2b319f1c8a4a
Anirudh Panda <anirudhpandaofficial@gmail.com>
2021-06-06 16:12:54 +0530

    comments added

--- extension.js
+++ extension.js
@@ -6,6 +6,7 @@ const xmlParser = require("fast-xml-parser");
  * @param {vscode.ExtensionContext} context
  */
 async function activate(context) {
+  // you can replace the rss.xml file with yours to get your blogs
   const res = await axios.get("https://anirudhpanda.hashnode.dev/rss.xml");
   const articles = xmlParser.parse(res.data).rss.channel.item.map((article) => {
     return {
