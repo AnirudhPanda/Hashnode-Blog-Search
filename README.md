ea7225933330cc5fbc37008d209951bff1675076
Anirudh Panda <anirudhpandaofficial@gmail.com>
2021-06-06 16:03:06 +0530

    vs code extension

---
+++ README.md
@@ -0,0 +1,37 @@
+# hashnode-blogs-search
+
+This extension lets you search for any of Anirudh Panda's blog articles from Hashnode from within VSCode.
+
+## How to?
+
+1. First open VSCode and press Ctrl+Shift+P on your keyboard(after installing the extension).
+2. Type in 'Search Hashnode Blog' in the space and press enter.
+3. Choose a Blog
+
+## Features
+
+Run the command Search Hashnode Blog and then search for any query to narrow the list of articles to your search.
+
+## Requirements/Dependencies
+
+1. Hashnode Blog Account
+2. npm and NodeJS
+3. VS Code API
+4. Axios Package
+5. Xml-Parser Package
+
+
+## Known Issues
+
+When `article.description` is used in the articles variable it returns the HTML markdown of the xml file instead of plain text.
+
+### 1.0.1
+
+Initial release on June 6 2021.
+
+## Contribute/Add Features
+
+This extension is open sourced here:
+https://github.com/AnirudhPanda/Hashnode-Blog-Search
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,42 @@
+const vscode = require("vscode");
+const axios = require("axios");
+const xmlParser = require("fast-xml-parser");
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+async function activate(context) {
+  const res = await axios.get("https://anirudhpanda.hashnode.dev/rss.xml");
+  const articles = xmlParser.parse(res.data).rss.channel.item.map((article) => {
+    return {
+      label: article.title,
+      link: article.link,
+    };
+  });
+  
+  let disposable = vscode.commands.registerCommand(
+    "hashnode-blogs-search.searchHashnodeBlog",
+    async function () {
+      const article = await vscode.window.showQuickPick(articles, {
+        matchOnDetail: true,
+      });
+
+      if (article == null) return;
+
+      vscode.env.openExternal(article.link);
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+exports.activate = activate
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+}
+
+
+
